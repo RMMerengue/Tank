dcbfda6b744e75482801141b90033a4749d16ab7
RMMerengue <1017703403@qq.com>
2021-11-25 10:10:55 +0800

    添加键盘处理，根据箭头的上下左右状态判断坦克的方向

--- src/com/msb/tank/TankFrame.java
+++ src/com/msb/tank/TankFrame.java
@@ -32,15 +32,54 @@ public class TankFrame extends Frame {
     }
 
     class MyKeyListener extends KeyAdapter {
+
+        boolean bL = false;
+        boolean bU = false;
+        boolean bR = false;
+        boolean bD = false;
+
         @Override
         public void keyPressed(KeyEvent e) {
-            x += 20;
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                default:
+                    break;
+            }
+//            x += 20;
 //            repaint();
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            System.out.println("key released");
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
