ea398f80db230800e01cbd901f8dbdf025bc4ed0
RMMerengue <1017703403@qq.com>
2021-11-25 09:28:55 +0800

    从Frame类继承，重写paint方法并画出一个黑方块

--- src/com/msb/tank/T.java
+++ src/com/msb/tank/T.java
@@ -6,17 +6,6 @@ import java.awt.event.WindowEvent;
 
 public class T {
     public static void main(String[] args) {
-        Frame f = new Frame();
-        f.setSize(800, 900);
-        f.setResizable(false);
-        f.setTitle("tanke war");
-        f.setVisible(true);
-
-        f.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        TankFrame tf = new TankFrame();
     }
 }
