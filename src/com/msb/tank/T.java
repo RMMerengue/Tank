4c63d007024aa1199fe7cae8f4d5680e1de7249d
RMMerengue <1017703403@qq.com>
2021-11-25 09:21:04 +0800

    new出一个窗口

---
+++ src/com/msb/tank/T.java
@@ -0,0 +1,22 @@
+package com.msb.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class T {
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.setSize(800, 900);
+        f.setResizable(false);
+        f.setTitle("tanke war");
+        f.setVisible(true);
+
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
